b73d495f4b3ba528991839e7d097dde12feb9732
xiaoming <1746534101@qq.com>
2017-10-30 18:30:14 +0800

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'antd/lib/button';
+
 
 class App extends Component {
   render() {
@@ -12,6 +14,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+            <Button type="primary">Button</Button>
         </p>
       </div>
     );
